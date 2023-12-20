2bb348e3907f2d5b78d4a559eacd3541a3255a08
Yassine <yassine.n13@gmail.com>
2023-12-20 13:40:04 +0100

    Commit et push initial projet

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Bienvenue sur Copy Writing Freak !</title>
+  <!--Lien pour la Favicon-->
+  <link rel="shortcut icon" href="assets/logo_copywritingfreak.png" type="image/x-icon">
+  <!--Links pour css et framework css-->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <header>
+    <a href="index.html">
+      <img src="assets/logo_copywritingfreak.png" alt="Logo de CopyWriting Freak" width="70px" height="70px" class="img-thumbnail">
+    </a>
+  </header>
+
+  <main>
+    <section>
+      <div class="container-fluid bg-light">
+        <div class="container-md">
+          <div class="row align-items-center justify-content">
+            <div class="p-4 text-center w-100">
+              <h1 class="text-uppercase fw-bold fs-1" style="color: orangered;">Merci pour cette inscription</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid bg-black text-light">
+        <div class="container-md">
+          <div class="align-items-center justify-content">
+            <div class="p-4 text-center w-100">
+              <h1 class="text-uppercase fw-bold fs-3">C'est quoi la suite ? </h1>
+            </div>
+
+            <div class="container-fluid bg-black text-light">
+              <div class="container-md">
+                <div class="row align-items-center justify-content">
+                  <div class="col" style="width: 50px;height:50px">
+                    <h4>step1</h4>
+                  </div>
+                  <div class="col" style="width: 50px;height:50px">
+                    <h4>step2</h4>
+                  </div>
+                  <div class="col" style="width: 50px;height:50px">
+                    <h4>step3</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  </main>
+
+  <footer class=" bg-black py-3 fs-7">
+    <ul class="nav justify-content-center border-bottom pb-2 mb-2">
+      <li class="nav-item">
+        <a href="https://www.etsitutelancais.fr/conditions-generales-vente" class="nav-link text-light" target="_blank">Conditions Générales de
+          Vente
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://www.etsitutelancais.fr/politique-de-confidentialite" class="nav-link text-light" target="_blank">Politique de
+          confidentialité
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="https://www.etsitutelancais.fr/mentions-legales" class="nav-link text-light" target="_blank">Mentions
+          légales</a>
+      </li>
+    </ul>
+
+    <p class="text-center text-light">&copy;Copyright 2022 ~ Copywriting Freak&#8482;</p>
+
+  </footer>
+
+  <script src="javascript/bootstrap.min.js"></script>
+
+</body>
